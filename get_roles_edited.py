2c78beb32a77c758576d7402c6842a7607662557
Lucie Choi <lucie0407@berkeley.edu>
2017-10-22 05:24:17 -0700

    finished scraping transactions. started anaylzing

--- get_roles_edited.py
+++ get_roles_edited.py
@@ -1,3 +1,5 @@
+import re
+
 ORACC_FILE = 'raw-data/p001.atf'
 DREHEM_P_IDS_FILE = 'drehem_p_ids.txt'
 
@@ -7,7 +9,7 @@ NUM_TEXTS = 25
 #	dictionary: {p_index: transaction}
 #	object transaction has p_index, source, receiver, 
 
-
+complete_transaction_ls = list()
 complete_drehem_p_sets = set()
 p_sets_of_interest = set()
 transaction_ls = list()
@@ -21,10 +23,16 @@ class Transaction:
 		self.people = set()
 
 		self.line = line;
+		# ls of lines
+
+		self.ls_lines_containing_PN = list()
+		# ls of lines containing PN
 
 	def __str__(self):
 		return 'P' + str(self.p_index) + '\nlines: ' + self.line 
 		# + '\n\t' + str(self.people)
+	def get_num_people(self):
+		return len(self.people)
 
 def get_p_index(line):
 	# line of the form '&P100259 = ...': return '100259'
@@ -37,9 +45,9 @@ def get_drehem_p_ids():
 			complete_drehem_p_sets.add(line[:-1])
 	return complete_drehem_p_sets
 
-def collect_p_id_of_interest():
+def collect_p_id_of_interest(file_name):
 	get_drehem_p_ids();
-	with open(ORACC_FILE) as input_file:
+	with open(file_name) as input_file:
 		count = 0
 		for line in input_file:
 			line = line.strip()# remove \n
@@ -50,7 +58,7 @@ def collect_p_id_of_interest():
 	# print( p_sets_of_interest)
 	return p_sets_of_interest
 
-def get_transactions(file_name):
+def get_transactions(file_name, p_id_set_sort=None):
 	
 	with open(file_name) as input_file:
 		currentTransaction = None;
@@ -61,19 +69,124 @@ def get_transactions(file_name):
 					p_index = get_p_index(line)
 					currentTransaction = Transaction(p_index, line)
 				else:
-					transaction_ls.append(currentTransaction) # add the transaction to the list
+					if p_id_set_sort is None or currentTransaction.p_index in p_id_set_sort:
+						transaction_ls.append(currentTransaction) # add the transaction to the list
+					
 
 					p_index = get_p_index(line)
 					currentTransaction = Transaction(p_index, line) # start a new transaction
 			else:
 				currentTransaction.line += "\n" + line;
-		transaction_ls.append(currentTransaction) # add the last transaction to the list
+
+		# add the last transaction to the list
+		if p_id_set_sort is None or currentTransaction.p_index in p_id_set_sort:
+			transaction_ls.append(currentTransaction)
+		
 	# print(len(transaction_ls))
 	# print(transaction_ls[-1])
 	return transaction_ls
 
-					
 
-				
-# collect_p_id_of_interest()
-get_transactions(ORACC_FILE);
+# with open(ORACC_FILE) as input_file:
+# 	for line in input_file:
+# 		line = line.strip()
+# 		if not re.match(r'^&P|^#lem|^\d+\.|^@|^#|^\$|^\d+ʾ\.|^=:|^\s+$|^$', line):
+# 			print(line);
+		# if re.match(r'^&P|^#lem|^\d+\.|^\d+ʾ\.', line):
+		# 	print(line)
+		# elif re.match(r'|^\s+$|^$', line):
+			#ignore
+		# elif re.match(r'^@|^#|^\$|')
+def clean_transaction(transaction):
+	# change the whole transaction.line into
+	# a list of important text, discarding unimportant lines
+	# transaction.line = ["1. ~~ #lem: ~~" ... ]
+
+	# print(transaction.p_index)
+	searchObj = re.findall(r'(\d+ʾ\..*\n#lem:.*|\d+\..*\n#lem:.*)', transaction.line)
+	
+	transaction.line = searchObj
+
+
+def get_PN(transaction):
+	# get the list of lines containing PN --> transaction.ls_lines_containing_PN
+	# get the set of PN --> transaction.people
+	txt,translit = None, None
+	for line in transaction.line:
+		if "PN" in line:
+			transaction.ls_lines_containing_PN.append(line)
+
+			
+	for line in transaction.ls_lines_containing_PN:
+		txt,translit = line.split("\n")
+		
+		translit = translit.replace("#lem: ","")
+		
+		translit, txt = translit.split(";"), txt.split(" ")[1:]
+
+		for index, word  in enumerate(translit):
+		    if "PN" in word:
+		        # print(translit, txt, index)
+
+		        transaction.people.add(txt[index])
+	# if len(transaction.people) == 0:
+	# 	print(transaction.line,txt, translit, transaction.p_index)
+
+	# print(transaction.people)
+
+
+def process_files():
+	#return a complete list of transactions out of all input files
+	global p_sets_of_interest
+	global transaction_ls
+	global complete_transaction_ls
+	i = 1; # oracc file number
+
+
+	
+	while i <= 15:
+		p_sets_of_interest = set()
+		transaction_ls = list()
+		if i < 10:
+			ORACC_FILE = 'raw-data/p00'+str(i)+'.atf'
+		else:
+			ORACC_FILE = 'raw-data/p0'+str(i)+'.atf'
+		collect_p_id_of_interest(ORACC_FILE);
+		get_transactions(ORACC_FILE, p_sets_of_interest);
+		for trans in transaction_ls:		
+			clean_transaction(trans)
+			get_PN(trans)
+		complete_transaction_ls += transaction_ls
+		# print(len(transaction_ls), len(p_sets_of_interest))
+		print("completed "+ORACC_FILE)
+		i+=1
+
+	print("***Total of ", len(complete_transaction_ls), " transactions.***")
+	return complete_transaction_ls
+
+def main():
+	ls = process_files()
+	no_PN_count = 0
+	contain_ki_count = 0
+	contain_subati_count = 0
+	contain_ragaba_count = 0
+	for trans in ls:
+		if trans.get_num_people() == 0:
+			no_PN_count+=1
+		line = ''.join(trans.line)
+		if "ki[place]" in line:
+			contain_ki_count += 1
+			
+		if "šu ba-ti" in line:
+			contain_subati_count += 1
+		if "ra₂-gaba" in line:
+			contain_ragaba_count += 1
+
+
+	print(no_PN_count, " transactions DO NOT have PN.")
+	print(contain_ki_count, " transactions contain a word ki[place].")
+	print(contain_subati_count, " transactions contain a word šu ba-ti.")
+	print(contain_ragaba_count, " transactions contain a word ra₂-gaba.")
+
+main()
+
