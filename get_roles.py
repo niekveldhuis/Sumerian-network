001f3cacf9f73e7884dabdc7af31d6de717c2fca
Tiffany Chien <tiffany.cubes@gmail.com>
2018-02-21 23:41:16 -0800

    Rewrote some of get_roles.py to be more organized. Removed dealing with transaction roles (temporarily)

--- get_roles.py
+++ get_roles.py
@@ -4,11 +4,13 @@ ORACC_FILES = ['raw-data/'+name for name in ['p001.atf', 'p002.atf', 'p003.atf',
 'p005.atf', 'p006.atf', 'p007.atf', 'p008.atf', 'p009.atf', 'p010.atf',
 'p011.atf', 'p012.atf', 'p013.atf', 'p014.atf', 'p015.atf']]
 DREHEM_P_IDS_FILE = 'drehem_p_ids.txt'
-PROFESSIONS_OUT = 'name_prof_pids.csv'
+OUTPUT_FILE = 'people.csv'
+PID_DATE_FILE = 'pid_to_datedecimal.csv'
 
 NUM_TEXTS = 20000
 
-NOT_PROFESSIONS = {'dab[seize]', 'maškim[administrator]', 'šu[hand]'}
+NOT_PROFESSIONS = {'dab[seize]', 'maškim[administrator]', 'šu[hand]', 'teŋ[approach]',
+					'ŋiri[foot]', 'X', 'FN'}
 
 class Transaction:
 	def __init__(self, p):
@@ -27,6 +29,14 @@ def get_drehem_p_ids():
 		for line in read_file:
 			p_sets.add(line[:-1])
 	return p_sets
+
+def get_pid_dates():
+	pid_date_d = {}
+	with open(PID_DATE_FILE) as input_file:
+		csv_reader = csv.reader(input_file)
+		for row in csv_reader:
+			pid_date_d[row[0]] = row[1]
+	return pid_date_d
 
 def get_p_index(line):
 	# line of the form '&P100259 = ...': return '100259'
@@ -65,22 +75,60 @@ def get_lems_and_words(lem_line, word_line):
 	# 	print('lem', lems)
 	return lems, words
 
+def process_PN(output_data, word_line, lem_line, p_index, date):
+	name = None
+	found_name = False
+
+	PN_info = {}
+
+	lem_line, word_line = get_lems_and_words(lem_line, word_line)
+
+	line_length = min(len(word_line), len(lem_line))
+	i = 0
+	while i < line_length:
+		word, lem = word_line[i], lem_line[i]
+		if lem == 'PN':
+			if not found_name:
+				name = word 
+				found_name = True
+		elif found_name:
+			if word == 'dumu' and i < line_length-1:
+				PN_info['dumu'] = word_line[i+1]
+				i += 1
+			elif 'profs' not in PN_info and lem not in NOT_PROFESSIONS:
+				PN_info['profs'] = lem
+		i += 1
+
+	# 3 times total in all the texts, 'PN' is the last lemma
+	# in the line, but the transliteration line is shorter
+	# than the lemmatization line
+		# ex. text 105224: '2(diš)', 'gin₂',        'ku₃',      'sa₁₀', 'a-ba-dingir-mu-gin₇'
+							# 'n', 'giŋ[unit]', 'kug[metal]', 'sa[pay', 'for]',     'PN'
+		# not sure what it means, but not important, probably.
+	if not name:
+		return
+
+	# if 'dumu' in PN_info and 'profs' in PN_info:
+	# 	print(name, PN_info['profs'], PN_info['dumu'], p_index)
+	# 	print(word_line, '|||', lem_line)
+
+	if 'profs' not in PN_info:
+		PN_info['profs'] = ''
+	if 'dumu' not in PN_info:
+		PN_info['dumu'] = ''
+
+	output_data.append([name, PN_info['profs'], PN_info['dumu'], p_index, date])
+
+
 def main():
 	drehem_texts = get_drehem_p_ids()
-
-	# professions = {}
-	# 	# dictionary from name -> lemmatization of profession
-	# 							# (word that comes right after name)
-
-	# count_num_names = 0
-	# num_trans_dict = {}
-	# 	# dictionary from name -> number of transactions they appear in
-
+	pid_date_dict = get_pid_dates()
 	all_professions = set()
 
-	name_prof_pids = []
+	output_data = []
+	count_texts = 0
 
-	count_texts, num_PNs = 0, 0
+	count_pids_without_dates = 0
 
 	for oracc_filename in ORACC_FILES:
 		with open(oracc_filename) as input_file:
@@ -98,50 +146,34 @@ def main():
 						break
 					curr_trans = Transaction(p_index)
 					all_trans[p_index] = curr_trans
-				elif line.startswith('#lem'):
-					# print(prev_line)
-					# print(line)
-					lems, words = get_lems_and_words(line, prev_line)
-					line_length = min(len(lems), len(words))
-					for i in range(line_length):
-						curr_lem, curr_word = lems[i], words[i]
-						if curr_word == 'mu':	# ignore names in year name
-							break
-						if curr_lem == 'PN':
-							num_PNs += 1
-							if curr_word[-3:] == '-ta':
-								curr_word = curr_word[:-3]
-							if i < line_length - 1 and lems[i+1] not in NOT_PROFESSIONS:
-								all_professions.add(lems[i+1])
-								name_prof_pids.append([curr_word, lems[i+1], p_index])
-								# if curr_word in professions:
-								# 	# professions[curr_word].add((words[i+1], lems[i+1]))
-								# 	professions[curr_word].add(lems[i+1])
-								# 	num_trans_dict[curr_word] += 1
-								# else:
-								# 	professions[curr_word] = set()
-								# 	# professions[curr_word].add((words[i+1], lems[i+1]))
-								# 	professions[curr_word].add(lems[i+1])
-								# 	num_trans_dict[curr_word] = 1
-								# print(curr_word, ':', professions[curr_word])
-							else:
-								name_prof_pids.append([curr_word, 'None', p_index])
-							curr_trans.people.add(curr_word)
-						# if curr_word == 'ki':
-						# 	# print(words[i+1], 'is a source')
-						# 	curr_trans.roles['source'] = words[i+1]
-					# print('')
+					curr_line_queue = []
+				if 'PN' in line:
+					if p_index in pid_date_dict:
+						date = pid_date_dict[p_index]
+					else:
+						count_pids_without_dates += 1
+						date = ''
+					process_PN(output_data, prev_line, line, p_index, date)
+
 				prev_line = line
 
-	# print(professions)
-	with open(PROFESSIONS_OUT, 'w') as output_file:
+	with open(OUTPUT_FILE, 'w') as output_file:
 		csv_writer = csv.writer(output_file)
-		csv_writer.writerow(['name', 'profession', 'p index'])
-		for row in name_prof_pids:
+		csv_writer.writerow(['name', 'profession', 'family (dumu)', 'p index', 'date'])
+		for row in sorted(output_data, key=lambda x: x[0]+str(x[-1])):		# sorted alphabetically by name, then date
 			csv_writer.writerow(row)
-		# for name, profession in sorted(professions.items(), key=lambda x: x[0]):
-		# 	csv_writer.writerow([name] + [str(profession)])
-	print(num_PNs, 'PNs found')
+
+	print(count_pids_without_dates, 'texts without dates?? out of', count_texts, 'total texts')
+
+	# print(professions)
+	# with open(PROFESSIONS_OUT, 'w') as output_file:
+	# 	csv_writer = csv.writer(output_file)
+	# 	csv_writer.writerow(['name', 'profession', 'p index'])
+	# 	for row in name_prof_pids:
+	# 		csv_writer.writerow(row)
+	# 	# for name, profession in sorted(professions.items(), key=lambda x: x[0]):
+	# 	# 	csv_writer.writerow([name] + [str(profession)])
+	# print(num_PNs, 'PNs found')
 
 	# print('number of PNs found vs. number of unique names:', count_num_names, len(professions))
 	# print('number of unique professions found:', len(all_professions))
